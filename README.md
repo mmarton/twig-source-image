5198b559f31ff656810ab8afd8a424c403a36dca
Misley Márton <misley.marton@webdream.hu>
2022-03-07 14:10:27 +0100

    POC
    - add logo
    - add fileloader
    - add twgig html extra
    - add controller
    - add source bug

---
+++ README.md
@@ -0,0 +1,9 @@
+## Start
+
+```bash
+composer install
+yarn install
+yarn dev
+php -S 127.0.0.1:8080 -t ./public/
+google-chrome 127.0.0.1:8080
+```
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('')]
+    public function __invoke(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
